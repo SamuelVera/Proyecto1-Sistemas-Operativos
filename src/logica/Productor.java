7969b447782286ec8af23fb79d35fe218248b59c
SamuelVera <samuelvera1999@gmail.com>
2018-10-10 23:13:42 -0400

    Agregado propiedades e inicializador del Productor

--- src/logica/Productor.java
+++ src/logica/Productor.java
@@ -2,12 +2,26 @@ package logica;
 
 import java.util.concurrent.Semaphore;
 
-public class Consumer extends Thread{
+public class Productor extends Thread{
     
-    private Semaphore s;
+    Almacen a;
+    private Semaphore sP; //Semáforo de productores
+    private Semaphore sC; //Semáforo consumidores
+    private Semaphore sE; //Semáforo de exclusividad
+    private Semaphore sI; //Semádoro para imprimir en la Interfaz
+    private int apunta; //Apuntador al búffer
+    private boolean val; //Valor que ingresa al búffer el proceso
+    int sleep; //Tiempo de espera
     
-    Consumer(Semaphore s){
-        this.s = s;
+    Productor(Almacen a, Semaphore sP, Semaphore sC, Semaphore sE, int apunta, boolean val, Semaphore sI, int sleep){
+        this.a = a;
+        this.sP = sP;
+        this.sC = sC;
+        this.sE = sE;
+        this.apunta = apunta;
+        this.val = val;
+        this.sI = sI;
+        this.sleep = sleep;
     }
     
     @Override
