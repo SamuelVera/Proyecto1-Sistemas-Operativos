28ae04da056db06dfd9c57456be236ba20ead1c9
SamuelVera <samuelvera1999@gmail.com>
2018-10-14 01:54:26 -0400

    Retoques menores, por añadir accesos al búffer

--- src/logica/Productor.java
+++ src/logica/Productor.java
@@ -1,10 +1,11 @@
 package logica;
 
 import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Productor extends Thread{
     
-    Almacen a;
     private Semaphore sP; //Semáforo de productores
     private Semaphore sC; //Semáforo consumidores
     private Semaphore sE; //Semáforo de exclusividad
@@ -12,9 +13,9 @@ public class Productor extends Thread{
     private int apunta; //Apuntador al búffer
     private boolean val; //Valor que ingresa al búffer el proceso
     int sleep; //Tiempo de espera
+    int prod; //Tipo de prodctor (0-Bat/1-Pan/2-Cab)
     
-    Productor(Almacen a, Semaphore sP, Semaphore sC, Semaphore sE, int apunta, boolean val, Semaphore sI, int sleep){
-        this.a = a;
+    Productor(Semaphore sP, Semaphore sC, Semaphore sE, int apunta, boolean val, Semaphore sI, int sleep, int prod){
         this.sP = sP;
         this.sC = sC;
         this.sE = sE;
@@ -22,11 +23,41 @@ public class Productor extends Thread{
         this.val = val;
         this.sI = sI;
         this.sleep = sleep;
+        this.prod = prod;
     }
     
     @Override
     public void run(){
-        
+        while(true){
+            if(this.prod == 0){
+                try {
+                //Productor de Baterías
+                Thread.sleep(this.sleep);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Productor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                System.out.println("Hola produzco Baterías");
+            }else if(this.prod == 1){ 
+                try {
+                //Productor de Pantallas
+                Thread.sleep(this.sleep);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Productor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                System.out.println("Hola produzco Pantallas");
+            }else if(this.prod == 2){ 
+                try {
+                //Productor de Cables
+                Thread.sleep(this.sleep);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Productor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+                System.out.println("Hola produzco Cables");
+            }
+        }
     }
     
 }
