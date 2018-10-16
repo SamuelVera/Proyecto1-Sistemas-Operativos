5b2ba5ce99969d3702ce91f15d215bef6c23ca17
SamuelVera <samuelvera1999@gmail.com>
2018-10-16 00:18:22 -0400

    Clases del Escritor/Lector

--- src/logica/Escritor.java
+++ src/logica/Escritor.java
@@ -1,10 +1,15 @@
 package logica;
+import java.util.concurrent.Semaphore;
     //Clase Escritor(Cronometrador)
-public class Escritor {
+public class Escritor extends Thread{
     
     private int sleep;
+    private Semaphore raceSema;
+    private int[] contador;
     
-    Escritor(int sleep){
+    Escritor(Semaphore raceSema, int[] contador, int sleep){
+        this.raceSema = raceSema;
+        this.contador = contador;
         this.sleep = sleep;
     }
     
--- src/logica/Lector.java
+++ src/logica/Lector.java
@@ -1,5 +1,16 @@
 package logica;
+import java.util.concurrent.Semaphore;
     //Clase de lector (Gerente)
-public class Lector {
+public class Lector extends Thread{
+    
+    private int sleep;
+    private Semaphore raceSema;
+    private int[] contador;
+    
+    Lector(Semaphore raceSema, int[] contador, int sleep){
+        this.raceSema = raceSema;
+        this.contador = contador;
+        this.sleep = sleep;
+    }
     
 }
