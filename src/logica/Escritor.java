b6042f193bb34907dc8ee50e1c03fe6eccc39be3
SamuelVera <samuelvera1999@gmail.com>
2018-10-17 18:35:38 -0400

    Clase del escritor/Cronómetro

--- src/logica/Escritor.java
+++ src/logica/Escritor.java
@@ -1,17 +1,57 @@
 package logica;
+import interfaz.Inicio;
 import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
     //Clase Escritor(Cronometrador)
 public class Escritor extends Thread{
     
     private int sleep;
-    private Semaphore raceSema;
+    private Semaphore raceSema, ceroSema, ceroSema2;
     private int[] contador;
+    private int diasD;
     
-    Escritor(Semaphore raceSema, int[] contador, int sleep){
+    public Escritor(Semaphore raceSema, Semaphore ceroSema, Semaphore ceroSema2, int[] contador, int sleep, int diasD){
         this.raceSema = raceSema;
+        this.ceroSema = ceroSema;
+        this.ceroSema2 = ceroSema2;
         this.contador = contador;
         this.sleep = sleep;
+        this.diasD = diasD;
     }
     
+    @Override
+    public void run(){
+        while(true){
+            try {
+                
+                    //Esperar todo el día menos la última 1.5 horas
+                Thread.sleep(this.sleep*15/16);
+                
+                raceSema.acquire(); //Sección de entrada al contador
+                Inicio.crono.setText("El cronometrador está revisando el contador");
+                if(contador[0] == 0){
+                    Inicio.crono.setText("El cronometrador está esperando por el gerente a despachar");
+                    ceroSema.acquire(); //Esperar por el gerente para coherencia de datos
+                    Thread.sleep(this.sleep/16); //Esperar 1.5 horas para disminuir contador
+                    contador[0] = this.diasD; //Reiniciar contador
+                    ceroSema2.release();
+                }else{
+                    Inicio.crono.setText("El cronometrador está disminuyendo el contador");
+                    Thread.sleep(this.sleep/16); //Esperar 1.5 horas para disminuir contador
+                    contador[0]--;
+                }
+                raceSema.release();//Sección de salida
+                
+                    //Impresiones
+                Inicio.diasD.setText("Días para el despacho: "+contador[0]);
+                Inicio.crono.setText("El cronometrador está dormido");
+                
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Escritor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } 
+    }
     
 }
